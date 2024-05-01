266246c0d0fc8e8e710e9bab5846ed51c57129b3
AntoineCuinet <antoine.cuinetdu70200@gmail.com>
2024-05-01 14:48:08 +0200

    add toString Grid

--- src/Grid.java
+++ src/Grid.java
@@ -1,3 +1,59 @@
 public class Grid {
-    
+    private int width;
+    private int height;
+
+    private String tour = "\u25A1";
+    private String tourPleine = "\u25A3";
+    private String perso = "\u263A";
+    private String mur = Main.ANSI_RED + "\u2588" + Main.ANSI_RESET;
+    private String murBas = Main.ANSI_RED + "\u2584" + Main.ANSI_RESET;
+    private String murHaut = Main.ANSI_RED + "\u2580" + Main.ANSI_RESET;
+
+
+
+    public Grid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Grid() {
+        this(10, 10);
+    }
+
+
+    @Override
+    public String toString() {
+        String res = "";
+        res += mur;
+        for (int j = 0; j < this.width; j++) {
+            res += murHaut + murHaut + murHaut + murHaut;
+        }
+        res += murHaut + mur + "\n";
+
+        for (int i = 0; i < this.height; i++) {
+            res += mur + " ";
+            for (int j = 1; j < this.width + 1; j++) {
+                res += " " + perso + " |";
+            }
+            res = res.substring(0, res.length() - 1);
+            res += " " + mur + "\n" + mur;
+
+            if (i < this.height - 1) {
+                for (int j = 0; j < this.width; j++) {
+                    res += " ---";
+                }
+            } else {
+                res = res.substring(0, res.length() - 17);
+            }
+            res += " " + mur + "\n";
+        }
+        
+        res += mur;
+        for (int j = 0; j < this.width; j++) {
+            res += murBas + murBas + murBas + murBas;
+        }
+        res += murBas + mur + "\n";
+
+        return res;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
+    // déclaration des couleurs utilisées pour l'affichage (pour la créativité)
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String YELLOW_BG = "\u001B[43m";
+    public static final String RESET_BG = "\u001B[40m";
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        
+        Grid grid = new Grid(20, 5);
+        System.out.println(grid);
         
     }
 }
